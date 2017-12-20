f973c284d73728ab5f563dec9567a6a96369f243
jiulongwang <564940585@qq.com>
2017-12-20 17:03:15 +0800

    预警信息中心，点击记录显示单条预警

---
+++ app/yt/js/pages/yjxxxq.js
@@ -0,0 +1,17 @@
+mui.init();
+mui.plusReady(initView);
+
+function initView () {
+	//页面传值
+	var self = plus.webview.currentWebview();
+	var warnId = self.warnId;
+	var action = "alarms/";
+	mui.myMuiQuery(action + warnId, null, function(result) {
+		var html = template('ul-li-template', {
+			data: result.data
+		});
+		document.getElementById("warn-Info").innerHTML = html;
+	}, function(reslut) {
+
+	});
+}
