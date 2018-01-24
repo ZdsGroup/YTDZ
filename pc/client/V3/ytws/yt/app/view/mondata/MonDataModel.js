07ed1e0381a991423f6e635c707b49705f9f5ffd
LyuNeil <372420759@qq.com>
2018-01-25 01:02:48 +0800

    监测点所有查询条件增加完成，并添加对应事件，地图设备点击事件、左侧树的联动

--- pc/client/V3/ytws/yt/app/view/mondata/MonDataModel.js
+++ pc/client/V3/ytws/yt/app/view/mondata/MonDataModel.js
@@ -5,10 +5,7 @@ Ext.define('yt.view.mondata.MonDataModel', {
     extend: 'Ext.app.ViewModel',
     alias: 'viewmodel.mondata',
 
-    requires: [
-        'Ext.data.proxy.Ajax',
-        'Ext.data.reader.Json'
-    ],
+    requires: [],
 
     stores: {
         /*
@@ -27,15 +24,6 @@ Ext.define('yt.view.mondata.MonDataModel', {
                 {name: '裂缝设备', type: 'lfjc'},
                 {name: '位移设备', type: 'wyjc'},
                 {name: '雨量设备', type: 'yljc'}
-            ]
-        },
-        areaStore: {
-            alias: 'store.areas',
-            data: [
-                {name: '市辖区', code: '600000000'},
-                {name: '月湖区', code: '300000000'},
-                {name: '余江县', code: '400000000'},
-                {name: '贵溪市', code: '500000000'}
             ]
         }
     },
