7a2318a340550b8e9fa47a5d4914431b4384c844
bmliang <liangbimiao@piesat.cn>
2018-02-11 11:22:16 +0800

    调整页面加载背景色

--- pc/client/V3/ytws/yt/app/controller/Global.js
+++ pc/client/V3/ytws/yt/app/controller/Global.js
@@ -180,7 +180,7 @@ var g = {
                     style: {
                         width: '100%',
                         height: '100%',
-                        background: '#0773A6'
+                        background: '#13A793'
                     }
                 });
                 loadMask.show();
